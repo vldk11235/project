8b032c5feba0293ad44b0cfa72e76e7d51e1c2ae
Vlad <vlad.kremenchuk@gmail.com>
2022-04-18 14:46:20 +0300

    javaScript_learning

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-console.log('arr' + " - object")
-console.log(4 + + "5")
+console.log('arr' + " - object");
+console.log(4 + "5");
 
 let incr = 10,
     decr = 10;
@@ -21,3 +21,24 @@ console.log(2 + 2 * 2  != 8);
 const isChecked = false,
     isClose = false;
 console.log(isChecked || !isClose);
+
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    a: 'a',
+    1: 'b',
+    2: 'c',
+    abc: {
+
+    }
+};
+
+arrObj.b = '1234';
+
+console.log(arrObj.b);
+
+// const obj = { a: 1, b: 2 };
+
+const obj = {
+    'Anna': 500,
+    'Alice': 800
+};
